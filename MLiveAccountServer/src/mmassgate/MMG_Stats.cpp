a71b3a0f6622681dd04bd0c0c08ba8ac25c67b6f
Nukem <Nukem@outlook.com>
2016-10-23 00:17:56 -0400

    MMG_Stats::ClanMatchStats/MMG_Stats::PlayerMatchStats

--- MLiveAccountServer/src/mmassgate/MMG_Stats.cpp
+++ MLiveAccountServer/src/mmassgate/MMG_Stats.cpp
@@ -279,6 +279,285 @@ bool MMG_Stats::ClanStatsRsp::FromStream(MN_ReadMessage *aMessage)
 		&& aMessage->ReadUInt(this->m_HighScoreAsArmor);
 }
 
+MMG_Stats::PlayerMatchStats::PlayerMatchStats()
+{
+	m_GameProtocolVersion				= 0;
+	m_ProfileId							= 0;
+	m_ScoreTotal						= 0;
+	m_ScoreAsInfantry					= 0;
+	m_ScoreAsSupport					= 0;
+	m_ScoreAsArmor						= 0;
+	m_ScoreAsAir						= 0;
+	m_ScoreByDamagingEnemies			= 0;
+	m_ScoreByUsingTacticalAids			= 0;
+	m_ScoreByCommandPointCaptures		= 0;
+	m_ScoreByRepairing					= 0;
+	m_ScoreByFortifying					= 0;
+	m_ScoreLostByKillingFriendly		= 0;
+	m_TimeTotalMatchLength				= 0;
+	m_TimePlayedAsUSA					= 0;
+	m_TimePlayedAsUSSR					= 0;
+	m_TimePlayedAsNATO					= 0;
+	m_TimePlayedAsInfantry				= 0;
+	m_TimePlayedAsAir					= 0;
+	m_TimePlayedAsArmor					= 0;
+	m_TimePlayedAsSupport				= 0;
+	m_MatchWon							= 0;
+	m_MatchType							= 0;
+	m_MatchLost							= 0;
+	m_MatchWasFlawlessVictory			= 0;
+	m_NumberOfUnitsKilled				= 0;
+	m_NumberOfUnitsLost					= 0;
+	m_NumberOfCommandPointCaptures		= 0;
+	m_NumberOfReinforcementPointsSpent	= 0;
+	m_NumberOfRoleChanges				= 0;
+	m_NumberOfTacticalAidPointsSpent	= 0;
+	m_NumberOfNukesDeployed				= 0;
+	m_NumberOfTacticalAidCriticalHits	= 0;
+	m_BestData							= 0;
+	m_TotalTimePlayed					= 0.0f;
+	m_WasPlayingAtMatchEnd				= 0;
+}
+
+void MMG_Stats::PlayerMatchStats::ToStream(MN_WriteMessage *aMessage)
+{
+	aMessage->WriteUInt(MassgateProtocolVersion);
+	aMessage->WriteUInt(this->m_ProfileId);
+	aMessage->WriteUShort(this->m_TimePlayedAsUSA);
+	aMessage->WriteUShort(this->m_TimePlayedAsUSSR);
+	aMessage->WriteUShort(this->m_TimePlayedAsNATO);
+	aMessage->WriteUShort(this->m_TimePlayedAsInfantry);
+	aMessage->WriteUShort(this->m_TimePlayedAsAir);
+	aMessage->WriteUShort(this->m_TimePlayedAsArmor);
+	aMessage->WriteUShort(this->m_TimePlayedAsSupport);
+	aMessage->WriteUShort(this->m_ScoreTotal);
+	aMessage->WriteUShort(this->m_ScoreAsInfantry);
+	aMessage->WriteUShort(this->m_ScoreAsSupport);
+	aMessage->WriteUShort(this->m_ScoreAsArmor);
+	aMessage->WriteUShort(this->m_ScoreAsAir);
+	aMessage->WriteUShort(this->m_ScoreByDamagingEnemies);
+	aMessage->WriteUShort(this->m_ScoreByRepairing);
+	aMessage->WriteUShort(this->m_ScoreByUsingTacticalAids);
+	aMessage->WriteUShort(this->m_ScoreByCommandPointCaptures);
+	aMessage->WriteUShort(this->m_ScoreByFortifying);
+	aMessage->WriteUShort(this->m_ScoreLostByKillingFriendly);
+	aMessage->WriteUShort(this->m_NumberOfUnitsKilled);
+	aMessage->WriteUShort(this->m_NumberOfUnitsLost);
+	aMessage->WriteUShort(this->m_NumberOfCommandPointCaptures);
+	aMessage->WriteUShort(this->m_NumberOfReinforcementPointsSpent);
+	aMessage->WriteUShort(this->m_NumberOfTacticalAidPointsSpent);
+	aMessage->WriteUShort(this->m_NumberOfNukesDeployed);
+	aMessage->WriteUShort(this->m_NumberOfTacticalAidCriticalHits);
+	aMessage->WriteUShort(this->m_NumberOfRoleChanges);
+	aMessage->WriteUShort(this->m_TimeTotalMatchLength);
+	aMessage->WriteUChar(this->m_MatchType);
+	aMessage->WriteUChar(this->m_MatchWon);
+	aMessage->WriteUChar(this->m_MatchLost);
+	aMessage->WriteUChar(this->m_MatchWasFlawlessVictory);
+	aMessage->WriteUChar(this->m_BestData);
+}
+
+bool MMG_Stats::PlayerMatchStats::FromStream(MN_ReadMessage *aMessage)
+{
+	// Validate protocol first
+	if (!aMessage->ReadUInt(this->m_GameProtocolVersion))
+		return false;
+
+	if (this->m_GameProtocolVersion != MassgateProtocolVersion)
+		return false;
+
+	return aMessage->ReadUInt(this->m_ProfileId)
+		&& aMessage->ReadUShort(this->m_TimePlayedAsUSA)
+		&& aMessage->ReadUShort(this->m_TimePlayedAsUSSR)
+		&& aMessage->ReadUShort(this->m_TimePlayedAsNATO)
+		&& aMessage->ReadUShort(this->m_TimePlayedAsInfantry)
+		&& aMessage->ReadUShort(this->m_TimePlayedAsAir)
+		&& aMessage->ReadUShort(this->m_TimePlayedAsArmor)
+		&& aMessage->ReadUShort(this->m_TimePlayedAsSupport)
+		&& aMessage->ReadUShort(this->m_ScoreTotal)
+		&& aMessage->ReadUShort(this->m_ScoreAsInfantry)
+		&& aMessage->ReadUShort(this->m_ScoreAsSupport)
+		&& aMessage->ReadUShort(this->m_ScoreAsArmor)
+		&& aMessage->ReadUShort(this->m_ScoreAsAir)
+		&& aMessage->ReadUShort(this->m_ScoreByDamagingEnemies)
+		&& aMessage->ReadUShort(this->m_ScoreByRepairing)
+		&& aMessage->ReadUShort(this->m_ScoreByUsingTacticalAids)
+		&& aMessage->ReadUShort(this->m_ScoreByCommandPointCaptures)
+		&& aMessage->ReadUShort(this->m_ScoreByFortifying)
+		&& aMessage->ReadUShort(this->m_ScoreLostByKillingFriendly)
+		&& aMessage->ReadUShort(this->m_NumberOfUnitsKilled)
+		&& aMessage->ReadUShort(this->m_NumberOfUnitsLost)
+		&& aMessage->ReadUShort(this->m_NumberOfCommandPointCaptures)
+		&& aMessage->ReadUShort(this->m_NumberOfReinforcementPointsSpent)
+		&& aMessage->ReadUShort(this->m_NumberOfTacticalAidPointsSpent)
+		&& aMessage->ReadUShort(this->m_NumberOfNukesDeployed)
+		&& aMessage->ReadUShort(this->m_NumberOfTacticalAidCriticalHits)
+		&& aMessage->ReadUShort(this->m_NumberOfRoleChanges)
+		&& aMessage->ReadUShort(this->m_TimeTotalMatchLength)
+		&& aMessage->ReadUChar(this->m_MatchType)
+		&& aMessage->ReadUChar(this->m_MatchWon)
+		&& aMessage->ReadUChar(this->m_MatchLost)
+		&& aMessage->ReadUChar(this->m_MatchWasFlawlessVictory)
+		&& aMessage->ReadUChar(this->m_BestData);
+}
+
+MMG_Stats::ClanMatchStats::ClanMatchStats()
+{
+	m_GameProtocolVersion		= 0;
+	m_MapHash					= 0;
+	memset(m_MapName, 0, sizeof(m_MapName));
+	m_ClanId					= 0;
+	m_MatchType					= 0;
+	m_DominationPercentage		= 0;
+	m_AssaultNumCommandPoints	= 0;
+	m_AssaultEndTime			= 0;
+	m_TowNumLinesPushed			= 0;
+	m_TowNumPerimiterPoints		= 0;
+	m_MatchWon					= 0;
+	m_IsTournamentMatch			= 0;
+	m_MatchWasFlawlessVictory	= 0;
+	m_NumberOfUnitsKilled		= 0;
+	m_NumberOfUnitsLost			= 0;
+	m_NumberOfNukesDeployed		= 0;
+	m_NumberOfTACriticalHits	= 0;
+	m_TimeAsUSA					= 0;
+	m_TimeAsUSSR				= 0;
+	m_TimeAsNATO				= 0;
+	m_TotalScore				= 0;
+	m_ScoreByDamagingEnemies	= 0;
+	m_ScoreByRepairing			= 0;
+	m_ScoreByTacticalAid		= 0;
+	m_ScoreByFortifying			= 0;
+	m_ScoreByInfantry			= 0;
+	m_ScoreBySupport			= 0;
+	m_ScoreByAir				= 0;
+	m_ScoreByArmor				= 0;
+	m_PerPlayerDataCount		= 0;
+	memset(m_PerPlayerData, 0, sizeof(m_PerPlayerData));
+}
+
+void MMG_Stats::ClanMatchStats::ToStream(MN_WriteMessage *aMessage)
+{
+	aMessage->WriteUInt(MassgateProtocolVersion);
+	aMessage->WriteUInt64(this->m_MapHash);
+	aMessage->WriteString(this->m_MapName);
+	aMessage->WriteUInt(this->m_ClanId);
+	aMessage->WriteUChar(this->m_MatchType);
+
+	switch (this->m_MatchType)
+	{
+	case 0:
+		aMessage->WriteUShort(this->m_DominationPercentage);
+		break;
+
+	case 1:
+		aMessage->WriteUChar(this->m_AssaultNumCommandPoints);
+		aMessage->WriteUInt(this->m_AssaultEndTime);
+		break;
+
+	case 2:
+		aMessage->WriteUChar(this->m_TowNumLinesPushed);
+		aMessage->WriteUChar(this->m_TowNumPerimiterPoints);
+		break;
+	}
+
+	aMessage->WriteUChar(this->m_MatchWon);
+	aMessage->WriteUChar(this->m_IsTournamentMatch);
+	aMessage->WriteUChar(this->m_MatchWasFlawlessVictory);
+	aMessage->WriteUInt(this->m_NumberOfUnitsKilled);
+	aMessage->WriteUInt(this->m_NumberOfUnitsLost);
+	aMessage->WriteUInt(this->m_NumberOfNukesDeployed);
+	aMessage->WriteUInt(this->m_NumberOfTACriticalHits);
+	aMessage->WriteUInt(this->m_TimeAsUSA);
+	aMessage->WriteUInt(this->m_TimeAsUSSR);
+	aMessage->WriteUInt(this->m_TimeAsNATO);
+	aMessage->WriteUInt(this->m_TotalScore);
+	aMessage->WriteUInt(this->m_ScoreByDamagingEnemies);
+	aMessage->WriteUInt(this->m_ScoreByRepairing);
+	aMessage->WriteUInt(this->m_ScoreByTacticalAid);
+	aMessage->WriteUInt(this->m_ScoreByFortifying);
+	aMessage->WriteUInt(this->m_ScoreByInfantry);
+	aMessage->WriteUInt(this->m_ScoreBySupport);
+	aMessage->WriteUInt(this->m_ScoreByAir);
+	aMessage->WriteUInt(this->m_ScoreByArmor);
+	aMessage->WriteUInt(this->m_PerPlayerDataCount);
+
+	for (uint i = 0; i < this->m_PerPlayerDataCount; i++)
+	{
+		aMessage->WriteUInt(this->m_PerPlayerData[i].m_ProfileId);
+		aMessage->WriteUShort(this->m_PerPlayerData[i].m_Score);
+		aMessage->WriteUChar(this->m_PerPlayerData[i].m_Role);
+	}
+}
+
+bool MMG_Stats::ClanMatchStats::FromStream(MN_ReadMessage *aMessage)
+{
+	// Validate protocol first
+	if (!aMessage->ReadUInt(this->m_GameProtocolVersion))
+		return false;
+
+	if (this->m_GameProtocolVersion != MassgateProtocolVersion)
+		return false;
+
+	if (!aMessage->ReadUInt64(this->m_MapHash) ||
+		!aMessage->ReadString(this->m_MapName, ARRAYSIZE(this->m_MapName)) ||
+		!aMessage->ReadUInt(this->m_ClanId) ||
+		!aMessage->ReadUChar(this->m_MatchType))
+		return false;
+
+	switch (this->m_MatchType)
+	{
+	case 0:
+		if (!aMessage->ReadUShort(this->m_DominationPercentage))
+			return false;
+		break;
+
+	case 1:
+		if (!aMessage->ReadUChar(this->m_AssaultNumCommandPoints) ||
+			!aMessage->ReadUInt(this->m_AssaultEndTime))
+			return false;
+		break;
+
+	case 2:
+		if (!aMessage->ReadUChar(this->m_TowNumLinesPushed) ||
+			!aMessage->ReadUChar(this->m_TowNumPerimiterPoints))
+			return false;
+		break;
+	}
+
+	if (!aMessage->ReadUChar(this->m_MatchWon) ||
+		!aMessage->ReadUChar(this->m_IsTournamentMatch) ||
+		!aMessage->ReadUChar(this->m_MatchWasFlawlessVictory) ||
+		!aMessage->ReadUInt(this->m_NumberOfUnitsKilled) ||
+		!aMessage->ReadUInt(this->m_NumberOfUnitsLost) ||
+		!aMessage->ReadUInt(this->m_NumberOfNukesDeployed) ||
+		!aMessage->ReadUInt(this->m_NumberOfTACriticalHits) ||
+		!aMessage->ReadUInt(this->m_TimeAsUSA) ||
+		!aMessage->ReadUInt(this->m_TimeAsUSSR) ||
+		!aMessage->ReadUInt(this->m_TimeAsNATO) ||
+		!aMessage->ReadUInt(this->m_TotalScore) ||
+		!aMessage->ReadUInt(this->m_ScoreByDamagingEnemies) ||
+		!aMessage->ReadUInt(this->m_ScoreByRepairing) ||
+		!aMessage->ReadUInt(this->m_ScoreByTacticalAid) ||
+		!aMessage->ReadUInt(this->m_ScoreByFortifying) ||
+		!aMessage->ReadUInt(this->m_ScoreByInfantry) ||
+		!aMessage->ReadUInt(this->m_ScoreBySupport) ||
+		!aMessage->ReadUInt(this->m_ScoreByAir) ||
+		!aMessage->ReadUInt(this->m_ScoreByArmor) ||
+		!aMessage->ReadUInt(this->m_PerPlayerDataCount))
+		return false;
+
+	for (uint i = 0; i < this->m_PerPlayerDataCount; i++)
+	{
+		if (!aMessage->ReadUInt(this->m_PerPlayerData[i].m_ProfileId) ||
+			!aMessage->ReadUShort(this->m_PerPlayerData[i].m_Score) ||
+			!aMessage->ReadUChar(this->m_PerPlayerData[i].m_Role))
+			return false;
+	}
+
+	return true;
+}
+
 MMG_Stats::MMG_Stats()
 {
 }
--- MLiveAccountServer/src/mmassgate/MMG_Stats.h
+++ MLiveAccountServer/src/mmassgate/MMG_Stats.h
@@ -114,6 +114,107 @@ public:
 		bool FromStream	(MN_ReadMessage *aMessage);
 	};
 	
+	class PlayerMatchStats : public MMG_IStreamable
+	{
+	public:
+		uint m_GameProtocolVersion;
+		uint m_ProfileId;
+		ushort m_ScoreTotal;
+		ushort m_ScoreAsInfantry;
+		ushort m_ScoreAsSupport;
+		ushort m_ScoreAsArmor;
+		ushort m_ScoreAsAir;
+		ushort m_ScoreByDamagingEnemies;
+		ushort m_ScoreByUsingTacticalAids;
+		ushort m_ScoreByCommandPointCaptures;
+		ushort m_ScoreByRepairing;
+		ushort m_ScoreByFortifying;
+		ushort m_ScoreLostByKillingFriendly;
+		ushort m_TimeTotalMatchLength;
+		ushort m_TimePlayedAsUSA;
+		ushort m_TimePlayedAsUSSR;
+		ushort m_TimePlayedAsNATO;
+		ushort m_TimePlayedAsInfantry;
+		ushort m_TimePlayedAsAir;
+		ushort m_TimePlayedAsArmor;
+		ushort m_TimePlayedAsSupport;
+		uchar m_MatchWon;
+		uchar m_MatchType;
+		uchar m_MatchLost;
+		uchar m_MatchWasFlawlessVictory;
+		ushort m_NumberOfUnitsKilled;
+		ushort m_NumberOfUnitsLost;
+		ushort m_NumberOfCommandPointCaptures;
+		ushort m_NumberOfReinforcementPointsSpent;
+		ushort m_NumberOfRoleChanges;
+		ushort m_NumberOfTacticalAidPointsSpent;
+		ushort m_NumberOfNukesDeployed;
+		ushort m_NumberOfTacticalAidCriticalHits;
+		uchar m_BestData;
+		float m_TotalTimePlayed;
+		uchar m_WasPlayingAtMatchEnd;
+
+	private:
+
+	public:
+		PlayerMatchStats();
+
+		void ToStream	(MN_WriteMessage *aMessage);
+		bool FromStream	(MN_ReadMessage *aMessage);
+	};
+
+	class ClanMatchStats : public MMG_IStreamable
+	{
+	public:
+		class PerPlayerData
+		{
+		public:
+			uint m_ProfileId;
+			ushort m_Score;
+			uchar m_Role;
+		};
+
+		uint m_GameProtocolVersion;
+		uint64 m_MapHash;
+		wchar_t m_MapName[256];
+		uint m_ClanId;
+		uchar m_MatchType;
+		ushort m_DominationPercentage;
+		uchar m_AssaultNumCommandPoints;
+		uint m_AssaultEndTime;
+		uchar m_TowNumLinesPushed;
+		uchar m_TowNumPerimiterPoints;
+		uchar m_MatchWon;
+		uchar m_IsTournamentMatch;
+		uchar m_MatchWasFlawlessVictory;
+		uint m_NumberOfUnitsKilled;
+		uint m_NumberOfUnitsLost;
+		uint m_NumberOfNukesDeployed;
+		uint m_NumberOfTACriticalHits;
+		uint m_TimeAsUSA;
+		uint m_TimeAsUSSR;
+		uint m_TimeAsNATO;
+		uint m_TotalScore;
+		uint m_ScoreByDamagingEnemies;
+		uint m_ScoreByRepairing;
+		uint m_ScoreByTacticalAid;
+		uint m_ScoreByFortifying;
+		uint m_ScoreByInfantry;
+		uint m_ScoreBySupport;
+		uint m_ScoreByAir;
+		uint m_ScoreByArmor;
+		uint m_PerPlayerDataCount;
+		PerPlayerData m_PerPlayerData[8];
+
+	private:
+
+	public:
+		ClanMatchStats();
+
+		void ToStream	(MN_WriteMessage *aMessage);
+		bool FromStream	(MN_ReadMessage *aMessage);
+	};
+
 private:
 
 public:
